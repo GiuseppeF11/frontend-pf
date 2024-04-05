d1885c0aa514cbb81977116f6fcbce8156690274
Soniamy <marianna.marzullo@gmail.com>
2024-04-05 19:08:17 +0200

    installing route.js and creating a page

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,26 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 
 export default {
-    data() {
-        return {
-
-        };
-    },
-    components: {
-        AppHeader,
-        AppMain,
-        AppFooter
-    },  
-    methods: {
-
-    }
-}
+  data() {
+    return {};
+  },
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+  methods: {},
+};
 </script>
 
 <template>
-    <h1>
-        Mia App
-    </h1>
-
-    <AppHeader />
-
-    <AppMain />
-
-    <AppFooter />
+  <AppHeader />
+  <router-view></router-view>
+  <AppFooter />
 </template>
 
 <style lang="scss">
-@use "assets/scss/main" as *;
-@import "assets/scss/partials/reset";
+@use 'assets/scss/main' as *;
+@import 'assets/scss/partials/reset';
 </style>
---
+++ src/pages/Homepage.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1>Categorie</h1>
+    <ul>
+      <li v-for="category in categories" :key="category.id">
+        {{ category.name }}
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
