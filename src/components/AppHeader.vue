4fd346c4f02e629c25057b76e82e9d45f0ebabad
LucaDiFede <146111756+LucaDiFede@users.noreply.github.com>
2024-04-08 18:55:32 +0200

    Add Login

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,7 +13,13 @@ export default {
 
 <template>
   <header>
-    <Cart />
+    <img src="C:\MAMP\htdocs\classe114\frontend-pf\logoscritta.png" height="120px;" style="padding-left: 40px;">
+    <div style="float: right; padding-top: 40px; padding-right: 50px;">
+      <a href="/">
+        <button style="border-radius: 25px; margin-right: 20px; background-color: transparent; color: white; padding: 5px;"><i class="bi bi-person"></i> Login</button>
+      </a>
+        <Cart />
+    </div>
   </header>
 </template>
 
