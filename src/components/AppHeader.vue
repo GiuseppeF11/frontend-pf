37a911e24b43e00ae5ddaf641d028285cb33c2e4
LucaDiFede <146111756+LucaDiFede@users.noreply.github.com>
2024-04-08 19:01:45 +0200

    Fixed Logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,7 +13,7 @@ export default {
 
 <template>
   <header>
-    <img src="C:\MAMP\htdocs\classe114\frontend-pf\logoscritta.png" height="120px;" style="padding-left: 40px;">
+    <img src="/public/img/logoscritta.png" height="120px;" style="padding-left: 40px;">
     <div style="float: right; padding-top: 40px; padding-right: 50px;">
       <a href="/">
         <button style="border-radius: 25px; margin-right: 20px; background-color: transparent; color: white; padding: 5px;"><i class="bi bi-person"></i> Login</button>
