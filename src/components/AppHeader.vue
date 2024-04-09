53c844315fdcf8d7001368544b7358bdfab9adbf
costanzaguala <costanzasilviaguala@gmail.com>
2024-04-09 09:38:33 +0200

    edit style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -24,17 +24,13 @@ export default {
         
         <div class="d-flex">
 
-		<router-link  to="/login"> 
-          <a class="button-style-3" href="/">
+          <a class="button-style-3" href=":">
             Sei gia un nostro partner?
           </a>
-		</router-link>
 
-		<router-link  to="/register"> 
           <a class="button-style-3" href="/">
             Diventa un nostro partner!
           </a>
-		</router-link>
           <Cart/>
         </div>
       </div>
