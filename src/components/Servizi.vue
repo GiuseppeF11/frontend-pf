f9649d16b49a4d6dabc0f424a474e4676b1e4d36
GiuseppeF11 <faillagiuseppe13@gmail.com>
2024-04-11 10:48:17 +0200

    edit icons

--- src/components/Servizi.vue
+++ src/components/Servizi.vue
@@ -2,34 +2,19 @@
   <main class="my-4">
     <div class="row text-center">
       <div class="col-12 col-md m-4">
-        <font-awesome-icon
-          :icon="['fas', 'burger']"
-          size="2xl"
-          style="color: #ffffff"
-          class="font"
-        />
+        <i class="fa-solid fa-burger font"> </i>
         <h5 class="title">Facile da ordinare</h5>
         <p class="subtitle m-3">Pochi passaggi per ordinare il cibo da casa.</p>
       </div>
 
       <div class="col-12 col-md m-3">
-        <font-awesome-icon
-          :icon="['fas', 'truck-fast']"
-          size="2xl"
-          style="color: #ffffff"
-          class="font"
-        />
+        <i class="fa-solid fa-truck-fast font"> </i>
         <h5 class="title">Consegna veloce</h5>
         <p class="subtitle m-4">Consegna sempre puntuale ancora più veloce.</p>
       </div>
 
       <div class="col-12 col-md m-3">
-        <font-awesome-icon
-          :icon="['fas', 'seedling']"
-          size="2xl"
-          style="color: #ffffff"
-          class="font"
-        />
+        <i class="fa-solid fa-seedling font"> </i> 
         <h5 class="title">Ottima qualità</h5>
         <p class="subtitle m-4">
           Oltre che alla velocità la nostra priorità è la qualità!
@@ -59,6 +44,7 @@
 .font {
   height: 60px;
   margin-bottom: 15px;
+  font-size: 60px;
 }
 
 i {
