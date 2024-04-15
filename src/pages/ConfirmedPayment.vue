5b897fe9c31cf58bf08ff713000c9a574d0c5da4
Soniamy <marianna.marzullo@gmail.com>
2024-04-15 11:54:59 +0200

    create page ConfirmedPayment

---
+++ src/pages/ConfirmedPayment.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="confirmation-message">
+          <h1>Pagamento confermato</h1>
+          <p>
+            Grazie per il tuo ordine! Il tuo pagamento è stato confermato e
+            l'ordine è stato preso in carico dal ristoratore.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  margin-top: 50px;
+}
+.confirmation-message {
+  text-align: center;
+}
+.confirmation-message h1 {
+  font-size: 2rem;
+  color: #bc3431;
+}
+.confirmation-message p {
+  font-size: 1.2rem;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -3,65 +3,67 @@ import Homepage from './pages/Homepage.vue';
 import CategoryDetail from './pages/CategoryDetail.vue';
 import RestaurantDetail from './pages/RestaurantDetail.vue';
 import FoodDetail from './pages/FoodDetail.vue';
-import Login  from './pages/Login.vue';
-import Register  from './pages/Register.vue';
+import Login from './pages/Login.vue';
+import Register from './pages/Register.vue';
 import Credentials from './pages/Credentials.vue';
 import PaymentPage from './pages/payment.vue';
+import ConfirmedPayment from './pages/ConfirmedPayment.vue';
 
 const router = createRouter({
-history: createWebHistory(),
-routes: [
-        {
-        path: '/',
-        name: 'home',
-        component: Homepage,
-        },
-        {
-        path: '/category/:categoryId',
-        name: 'Category',
-        component: CategoryDetail,
-        },
-        {
-        path: '/restaurant/:restaurantId',
-        name: 'Restaurant',
-        component: RestaurantDetail,
-        },
-        {
-        path: '/food/:foodId',
-        name: 'Food',
-        component: FoodDetail,
-        },
-        {
-        path: '/login',
-        name: 'Login',
-        component: Login,
-        },
-        {
-        path: '/register',
-        name: 'Register',
-        component: Register,
-        },
-        {
-        path: '/credentials',
-        name: 'credentials',
-        component: Credentials,
-        },
-        {
-            path: '/payment',
-            name: 'payment',
-            component: PaymentPage,
-        },
-        /*
-        {
-            path: '/confirmed',
-            name: 'confirmed',
-            component: ConfirmedPayment,
-        },
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Homepage,
+    },
+    {
+      path: '/category/:categoryId',
+      name: 'Category',
+      component: CategoryDetail,
+    },
+    {
+      path: '/restaurant/:restaurantId',
+      name: 'Restaurant',
+      component: RestaurantDetail,
+    },
+    {
+      path: '/food/:foodId',
+      name: 'Food',
+      component: FoodDetail,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+    },
+    {
+      path: '/credentials',
+      name: 'credentials',
+      component: Credentials,
+    },
+    {
+      path: '/payment',
+      name: 'payment',
+      component: PaymentPage,
+    },
+
+    {
+      path: '/confirmedpayment',
+      name: 'ConfirmedPayment',
+      component: ConfirmedPayment,
+    },
+    /*
         {
             path: '/rejected',
             name: 'rejected',
             component: RejectedPayment,
         } */
-    ],
+  ],
 });
 export { router };
