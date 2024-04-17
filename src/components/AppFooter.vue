92bb118c7d33e21cd1bea6830515584460d76496
costanzaguala <costanzasilviaguala@gmail.com>
2024-04-17 13:34:45 +0200

    footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,18 +8,18 @@ export default {
 </script>
 
 <template>
-    <footer class="p-2">
-        <div class="container py-5">
-			<div class="row d-flex align-items-center pb-4">
-				<div class="col">
-					<div class="logo-container">
-						<img class="logo" src="/public/img/logo.png" alt="logo">
+	<footer class="p-2">
+		<div class="container py-5 d-flex flex-column align-items-center">
+			<div class="row pb-4 d-flex justify-content-center align-items-center">
+				<div class="col-6 d-flex justify-content-center align-items-center">
+					<div class="logo-container d-flex justify-content-center align-items-center">
+						<img class="logo-footer" src="/public/img/logo.png" alt="logo">
 					</div>
 				</div>
 			</div>
 			<div class="row pb-4">
-				<div class="col">
-					<div class="icons d-flex">
+				<div class="col d-flex justify-content-center">
+					<div class="icons d-flex justify-content-center">
 						<i class="fa-brands fa-twitter mx-2"></i> 
 						<i class="fa-brands fa-linkedin mx-2"></i> 
 						<i class="fa-brands fa-youtube mx-2"></i> 
@@ -29,66 +29,67 @@ export default {
 				</div>
 			</div>
 			<div class="row">
-				<div class="col">
-					<div class="text-footer mb-4">
+				<div class="col d-flex justify-content-center">
+					<div class="text-footer mb-4 text-center">
 						<strong>© FastBites • <a href="#" class="text-footer"> Cookie & Privacy Policy</a> • Developed by Team 4</strong>
 					</div>
 				</div>
 			</div>
-        </div>
-    </footer>
+		</div>
+	</footer>
 </template>
+  
+  <style lang="scss" scoped>
 
-<style lang="scss" scoped>
+  footer {
+	background-color: #F6901E;
+	width: 100%;
+	padding: 60px 0;
+	bottom: 0; 
+  }
+  
+  .text-footer {
+	font-family: 'Open Sans', sans-serif;
+	color: white;
+	font-size: 0.9rem;
+  }
 
-footer {
-  background-color: #F6901E;
+  .logo-footer {
   width: 100%;
-  padding: 60px 0;
-  bottom: 0; 
 }
 
-.text-footer {
-  font-family: 'Open Sans', sans-serif;
-  color: white;
-  font-size: 0.9rem;
-}
-
-.logo-container {
-  width: 100px;
-  height: 100px;
+  
+  .logo-container {
+  width: 250px;
   display: flex; 
   align-items: center; 
 }
 
-.logo {
-  width: 100%;
-}
-
-.icons {
-  width: 60px;
-  display: flex;
-  align-items: center;
-}
-
-.icons i {
-  border-radius: 25%;
-  background-color: white;
-  width: 60px;
-  height: 50px;
-  padding: 16px; 
-  border: 1px solid white;
-  color: #F6901E;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: background-color 0.5s, color 0.5s;
-  cursor: pointer;
-}
-
-.icons i:hover {
-  background-color: #F6901E;
-  color: white;
-}
-
-</style>
+  .icons {
+	width: 60px;
+	display: flex;
+	align-items: center;
+	justify-content: center; /* Center horizontally */
+  }
+  
+  .icons i {
+	border-radius: 25%;
+	background-color: white;
+	width: 60px;
+	height: 50px;
+	padding: 16px; 
+	border: 1px solid white;
+	color: #F6901E;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	transition: background-color 0.5s, color 0.5s;
+	cursor: pointer;
+  }
+  
+  .icons i:hover {
+	background-color: #F6901E;
+	color: white;
+  }
+  </style>
+  
